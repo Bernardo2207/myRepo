5b0ae80874fe3f28be9a9c9e70a1789e00a4d776
Bernardo Jr. Sein Acevedo <bernardo.sein@upr.edu>
2018-02-23 17:50:07 -0400

    Area calculation

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+package test;
+
+import java.util.Scanner;
+
 public class Main {
-public static void main(String args[]){
 
-int r=5;
-int area=0;
-
-area=3.14*r*r;
-
-System.out.println(area);
-
-}
+	public static void main(String args[]) {
+		
+		System.out.println("Enter raidus of Circle:");
+		Scanner sc= new Scanner(System.in);
+		//Radius of triangle.
+		
+		double radius=sc.nextDouble();
+	    double area= Math.PI*(radius*radius);
+	
+		
+		System.out.println("Area of the circle is: "+area);
+	}
 }
